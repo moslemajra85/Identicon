94d775553e4b046ef9c958d4c3c96f22eaec228c
Moslem Ajra <ajra.engineering@gmail.Com>
2023-12-18 08:12:43 +0100

    Add filter_odd_square function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,17 +4,43 @@ defmodule Identicon do
   """
 
   def main(input) do
-      input |> hash_input() |> pick_color() |> build_grid()
+      input
+      |> hash_input()
+      |> pick_color()
+      |> build_grid()
+      |> filter_odd_squares()
   end
 
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+
+   grid = Enum.filter grid, fn ({code, _index}) ->
+
+      rem(code, 2) == 0
+
+    end
+
+    %Identicon.Image{image | grid: grid}
+
+  end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
 
-        hex |> Enum.chunk_every(3, 3, :discard) |> mirro_row()
+       grid =
+         hex
+         |> Enum.chunk_every(3, 3, :discard)
+         |> Enum.map(&mirro_row/1)
+         |> List.flatten()
+         |> Enum.with_index()
+
+         %Identicon.Image{image | grid: grid}
   end
 
-  def mirro_row(chunks) do
-    for chunk <- chunks, [first, second | _third] = chunk do chunk ++ [second, first] end
+  def mirro_row(chunk) do
+    # for chunk <- chunks, [first, second | _third] = chunk do chunk ++ [second, first] end
+
+    [first, second | _third] = chunk
+    chunk ++ [second, first]
   end
 
   def pick_color(   %Identicon.Image{hex: [r, g,b | _tail]} = image) do
